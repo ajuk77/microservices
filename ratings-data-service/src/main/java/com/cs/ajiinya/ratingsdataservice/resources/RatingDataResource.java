1e2f0adc202165cd0a590ad527169e9bd5957de6
ajinkya.kulkarni77@gmail.com <ajinkya.kulkarni77@gmail.com>
2019-03-19 09:16:14 -0700

    Managed to call movie-info-service and ratings-data-service rest calls from movie-catalog-service

--- ratings-data-service/src/main/java/com/cs/ajiinya/ratingsdataservice/resources/RatingDataResource.java
+++ ratings-data-service/src/main/java/com/cs/ajiinya/ratingsdataservice/resources/RatingDataResource.java
@@ -1,19 +1,32 @@
 package com.cs.ajiinya.ratingsdataservice.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cs.ajiinya.ratingsdataservice.models.Rating;
+import com.cs.ajiinya.ratingsdataservice.models.UserRating;
 
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingDataResource {
-	
+
 	@RequestMapping("/{movieId}")
 	public Rating getRating(@PathVariable("movieId") String movieId) {
 		return new Rating(movieId, 4);
 	}
-	
+
+	@RequestMapping("/users/{userId}")
+	public UserRating getRatingsForUsers(@PathVariable("userId") String userId) {
+		List<Rating> response = Arrays.asList(new Rating("1234", 4), new Rating("5679", 5));
+
+		UserRating rating = new UserRating();
+		rating.setUserRating(response);
+		return rating;
+
+	}
 
 }
